47047ec1e28fc8f7a7d12a19f960b7d2a3e7bf6f
EmmanuelC40 <emmanuelcano21212121@gmail.com>
2025-05-14 17:44:15 -0400

    Create login_admin.php

---
+++ login_admin.php
@@ -0,0 +1,21 @@
+<?php session_start(); ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Admin Login</title>
+</head>
+<body style="background-color: #f4f4f4; padding: 20px;">
+  <form method="POST" action="process_admin.php" style="background: #ddd; padding: 50px; max-width: 400px; margin: auto; border: 4px solid #aaa;">
+    <h2 style="text-align:center;">Admin Login</h2>
+    
+    <label><b>Username:</b></label><br>
+    <input type="text" name="username" required style="width: 95%; padding: 7px;"><br><br>
+
+    <label><b>Password:</b></label><br>
+    <input type="password" name="password" required style="width: 95%; padding: 7px;"><br><br>
+
+    <input type="submit" value="Login" style="padding: 10px 20px; background: #aaa; border: none; cursor: pointer; width: 100%;">
+  </form>
+</body>
+</html>
